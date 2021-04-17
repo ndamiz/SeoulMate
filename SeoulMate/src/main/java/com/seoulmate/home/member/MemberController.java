4d5291b81255e142776d4b7ce45f75646ab95216
0905cjw <0905cjw@gmail.com>
2021-04-17 16:49:39 +0900

    회원가입(회원 정보, 가입 유형, 성향-생활)

--- SeoulMate/src/main/java/com/seoulmate/home/member/MemberController.java
+++ SeoulMate/src/main/java/com/seoulmate/home/member/MemberController.java
@@ -13,7 +13,9 @@ public class MemberController {
 		ModelAndView mav=new ModelAndView();
 		Calendar now=Calendar.getInstance();
 		int year=now.get(Calendar.YEAR);
+		String arr1[] = {"010"," 02"," 031","032"," 033"," 041"," 042"," 043"," 044"," 051"," 052"," 053"," 054"," 055"," 061"," 062"," 063"," 064"};
 		
+		mav.addObject("arr1", arr1);
 		mav.addObject("year", year);
 		mav.setViewName("member/memberForm");
 		
