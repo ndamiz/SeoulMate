23e22bca0d0a8c23c3050ccb2ed92e38e1eda045
leculo90 <leculo90@gmail.com>
2021-04-20 14:29:28 +0900

    2020.14.29

--- SeoulMate/src/main/java/com/seoulmate/home/member/MemberController.java
+++ SeoulMate/src/main/java/com/seoulmate/home/member/MemberController.java
@@ -5,6 +5,9 @@ import javax.servlet.http.HttpSession;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController {
