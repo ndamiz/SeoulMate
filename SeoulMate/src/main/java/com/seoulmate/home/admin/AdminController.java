af3884a97335245c1f7fd05a66eb1ec6f1cd1862
leculo90 <leculo90@gmail.com>
2021-04-22 15:31:23 +0900

    .

--- SeoulMate/src/main/java/com/seoulmate/home/admin/AdminController.java
+++ SeoulMate/src/main/java/com/seoulmate/home/admin/AdminController.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,6 +12,18 @@ public class AdminController {
 	@Autowired
 	SqlSession sqlSession;
 	
+	//admin에 들어오면 나오는 대시보드
+	@RequestMapping("/admin")
+	public String adminDashboard() {
+		return "/admin/adminDashboard";
+	}
+	
+	//신고관리
+	@RequestMapping("/admin/report")
+	public String adminReport() {
+		return "/admin/report";
+	}
+		
 	@RequestMapping(value="/admin/memberManagement", method={RequestMethod.POST, RequestMethod.GET})
 	public ModelAndView memberManagement() {
 		ModelAndView mav = new ModelAndView();
@@ -53,25 +64,3 @@ public class AdminController {
 		return mav;
 	}
 }
-
-=======
-
-@Controller
-public class AdminController {
-	@Autowired
-	SqlSession sqlSession;
-	
-	//admin에 들어오면 나오는 대시보드
-	@RequestMapping("/admin")
-	public String adminDashboard() {
-		return "/admin/adminDashboard";
-	}
-	
-	//신고관리
-	@RequestMapping("/admin/report")
-	public String adminReport() {
-		return "/admin/report";
-	}
-	
-}
->>>>>>> refs/remotes/origin/min
