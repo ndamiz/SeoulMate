62ad63527b3d422e706d16a12de439adabba0827
leculo90 <leculo90@gmail.com>
2021-05-20 14:40:04 +0900

    20210520

--- SeoulMate/src/main/java/com/seoulmate/home/controller/MateController.java
+++ SeoulMate/src/main/java/com/seoulmate/home/controller/MateController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.seoulmate.home.service.HomeService;
